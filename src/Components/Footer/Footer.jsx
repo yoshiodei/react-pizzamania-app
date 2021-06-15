4909ed8368070cd965151200a5f4c11ba44fecd7
yoshiodei <yoshiodei@yahoo.com>
2021-06-15 13:19:03 +0000

    add Footer Component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <footer>
+       <p class="text-center">&copy; 2021 Pizzamania All Rights Reserved!</p>   
+  </footer>
+    );
+}
+
+export default Footer;
