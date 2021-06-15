56f23ac20aa1349704fa4129cf44281fe2755d77
yoshiodei <yoshiodei@yahoo.com>
2021-06-15 13:17:23 +0000

    add About Component

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,20 @@
+import image3 from '../../images/img3.jpg';
+
+const About = () => {
+    return(
+        <div class="container-fluid">
+    <div class="row px-3">
+    <h2 class="text-center">About</h2>
+    <div class="col-md-6">
+        <img src={image3} class="img2" />
+    </div>
+    <div class="col-md-6">
+          <h3>Awesome Pizza</h3>
+          nesciunt et, ducimus velit placeat eaque laborum totam cum? Culpa impedit fugit modi, accusamus quis quidem provident, autem magni minima architecto, rerum cumque. Culpa qui, quia magnam laborum iste sapiente deserunt consequatur hic sequi nobis fugiat ipsa perspiciatis adipisci ea tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo porro asperiores a aliquid! Nam accusamus vel ipsum numquam, minus iure inventore sapiente dolore perspiciatis ea qui officia incidunt nemo eos voluptas deserunt quia illo ipsam modi repudiandae officiis consequuntur nulla accusantium consequatur! Quisquam quos dolor sequi ipsam sint eius quo nulla quae perspiciatis accusantium excepturi quam dolore, laudantium fuga nesciunt et, ducimus velit placeat eaque laborum totam cum? Culpa impedit fugit modi, accusamus quis quidem provident, autem magni minima  accusamus quis quidem provident, autem magni minima nulla accusantium consequatur! Quisquam quos dolor sequi ipsam sint eius quo nulla quae perspiciatis accusantium excepturi quam dolore, laudantium fuga nesciunt et, ducimus velit placeat eaque laborum totam cum? Culpa impedit fugit modi, accusamus quis quidem provident, autem magni minima  accusamus quis quidem provident, autem magni minima 
+    </div>
+</div>
+</div>
+    );
+}
+
+export default About;
