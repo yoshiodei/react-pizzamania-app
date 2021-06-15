4757820b15693e38df3b3f57ab23959d34454e63
yoshiodei <yoshiodei@yahoo.com>
2021-06-15 13:16:54 +0000

    add Carousel Component

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,33 @@
+import image2 from '../../images/img2.jpg';
+
+const Carousel = () => {
+    return(
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src={image2} class=" image" />
+    </div>
+    <div class="item">
+      <img src={image2} class=" image" /> 
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    );
+}
+
+export default Carousel;
