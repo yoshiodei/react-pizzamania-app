775df47f8884684d3760999144cb31f18e15fc45
yoshiodei <yoshiodei@yahoo.com>
2021-06-15 13:16:17 +0000

    add Navbar Component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+const Navbar = () => {
+    return(
+      <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">PIZZAMANIA</a>
+        </div>
+
+        
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Pizza</a></li>
+            <li><a href="#">Pizza Types</a></li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+    );
+}
+
+export default Navbar;
